827d37122afccdb0d49944927b9993936c9519e2
stmacebal <mateo.cebquerol@gmail.com>
2022-07-23 08:50:08 +0200

    Adding Day3Exercise2_game\myCode\main.h on 2022-07-23

---
+++ Day3Exercise2_game/myCode/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: 8 Sep 2021
+ *      Author: Mateo C. Querol
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+void clearboard(char x, char y, char c, unsigned int * mem);
+void printboard(char x, char y, unsigned int * mem);
+void setboard(char x, char y, char c, unsigned int * mem);
+
+
+
+#endif /* MAIN_H_ */
